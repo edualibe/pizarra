1e9c27f0a46c9a9a77a56604cd37c184d89be86a
Enrique <enriquedualibe@gmail.com>
2019-06-10 16:00:35 -0300

    10-6-19

--- src/sockets.js
+++ src/sockets.js
@@ -37,60 +37,62 @@ function nueva_conexion(socket){
             let id = url_param.replace('/suc/','');
             return parseInt(id);
         };
-        //hace consulta de datos a bd
-        const data = await pool.query('SELECT * FROM header WHERE sucursal_id = ?', [id_suc(data_url.url)]);
-        if (data.length>0){
-            //crear objeto json con datos encontrados
-                const row_img_1 = (data[0].header_content_1).split(',');
-                const row_img_2 = (data[0].header_content_2).split(',');
-                const row_img_3 = (data[0].header_content_3).split(',');
+        //trycatch para conrolar error en ruta /admin
+        try {
+            //hace consulta de datos a bd
+            const data = await pool.query('SELECT * FROM header WHERE sucursal_id = ?', [id_suc(data_url.url)]);
+            if (data.length>0){
+                //crear objeto json con datos encontrados
+                    const row_img_1 = (data[0].header_content_1).split(',');
+                    const row_img_2 = (data[0].header_content_2).split(',');
+                    const row_img_3 = (data[0].header_content_3).split(',');
+                    var data_encabezado = {
+                    "thead1": data[0].header_title1,
+                    "thead2": data[0].header_title2,
+                    "thead3": data[0].header_title3,
+                    "sub_thead1_1": data[0].header_subtitle1_1,
+                    "sub_thead1_2": data[0].header_subtitle2_1,
+                    "sub_thead2_1": data[0].header_subtitle2_1,
+                    "sub_thead2_2": data[0].header_subtitle2_2,
+                    "sub_thead3_1": data[0].header_subtitle3_1,
+                    "sub_thead3_2": data[0].header_subtitle3_2,
+                    "valor1_1": data[0].header_value1_1,
+                    "valor1_2": data[0].header_value1_2,
+                    "valor2_1": data[0].header_value2_1,
+                    "valor2_2": data[0].header_value2_2,
+                    "valor3_1": data[0].header_value3_1,
+                    "valor3_2": data[0].header_value3_2,
+                    "img_1": row_img_1,
+                    "img_2": row_img_2,
+                    "img_3": row_img_3
+                };
+            } else {
+                //crea objeto json con valores vacios
                 var data_encabezado = {
-                "thead1": data[0].header_title1,
-                "thead2": data[0].header_title2,
-                "thead3": data[0].header_title3,
-                "sub_thead1_1": data[0].header_subtitle1_1,
-                "sub_thead1_2": data[0].header_subtitle2_1,
-                "sub_thead2_1": data[0].header_subtitle2_1,
-                "sub_thead2_2": data[0].header_subtitle2_2,
-                "sub_thead3_1": data[0].header_subtitle3_1,
-                "sub_thead3_2": data[0].header_subtitle3_2,
-                "valor1_1": data[0].header_value1_1,
-                "valor1_2": data[0].header_value1_2,
-                "valor2_1": data[0].header_value2_1,
-                "valor2_2": data[0].header_value2_2,
-                "valor3_1": data[0].header_value3_1,
-                "valor3_2": data[0].header_value3_2,
-                "img_1": row_img_1,
-                "img_2": row_img_2,
-                "img_3": row_img_3
-            };
-        } else {
-            //crea objeto json con valores vacios
-            var data_encabezado = {
-                "thead1": "",
-                "thead2": "",
-                "thead3": "",
-                "sub_thead1_1": "",
-                "sub_thead1_2": "",
-                "sub_thead2_1": "",
-                "sub_thead2_2": "",
-                "sub_thead3_1": "",
-                "sub_thead3_2": "",
-                "valor1_1": "",
-                "valor1_2": "",
-                "valor2_1": "",
-                "valor2_2": "",
-                "valor3_1": "",
-                "valor3_2": "",
-                "img_1": "",
-                "img_2": "",
-                "img_3": ""
-            };
+                    "thead1": "",
+                    "thead2": "",
+                    "thead3": "",
+                    "sub_thead1_1": "",
+                    "sub_thead1_2": "",
+                    "sub_thead2_1": "",
+                    "sub_thead2_2": "",
+                    "sub_thead3_1": "",
+                    "sub_thead3_2": "",
+                    "valor1_1": "",
+                    "valor1_2": "",
+                    "valor2_1": "",
+                    "valor2_2": "",
+                    "valor3_1": "",
+                    "valor3_2": "",
+                    "img_1": "",
+                    "img_2": "",
+                    "img_3": ""
+                };
+            }
+        } catch (error) {
+            console.log(error.error);
         }
         //emite nuevo evento al cliente pasandole los datos encontrados en la bd para que los muestre en el navegador
         socket.emit('pagina:cargar', data_encabezado);
-    });
-
-
-
+    });            
 };
